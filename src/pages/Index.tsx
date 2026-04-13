4e1e53ca5614cab6e4288f95cbe21cfca8e68fd6
Sundar Ranganathan <sundarranganathan@Haris-MacBook-Air.local>
2026-04-13 15:49:40 +0530

    fix: remove duplicate react text overlays since typography is baked into video frames

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,6 @@ import EventsSection from "@/components/wedding/EventsSection";
 import CoupleSection from "@/components/wedding/CoupleSection";
 import CountdownSection from "@/components/wedding/CountdownSection";
 import Footer from "@/components/wedding/Footer";
-import FloatingLamps from "@/components/wedding/FloatingLamps";
 
 const Index = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -46,9 +45,6 @@ const Index = () => {
     <div ref={containerRef} className="relative">
       {/* Scroll progress bar */}
       <div id="scroll-progress" className="scroll-progress" style={{ width: "0%" }} />
-
-      {/* Floating lamps */}
-      <FloatingLamps />
 
       {/* Sections */}
       <HeroSection />
